1fb8a5911f869faa473097be1dfb188669fb9a72
sakagawakyohei <lamgiabao1039@gmail.com>
2025-01-21 22:07:14 +0700

    full text editor

--- pages/api/product/add-product.js
+++ pages/api/product/add-product.js
@@ -1,13 +1,27 @@
 import { AddProduct } from "@/app/lib/actions";
 
+// Cấu hình để tăng giới hạn kích thước body lên 10MB
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb', // Tăng giới hạn body lên 10MB
+    },
+  },
+};
+
 export default async function handler(req, res) {
-  const { product, uid} = req.body;
+  // Lấy dữ liệu từ body
+  const { product, uid } = req.body;
+
   try {
+    // Gọi hàm AddProduct
     await AddProduct(product, uid);
-    res.status(200).json({ message: 'Delete Partner successfully' });
-    console.log(hangid)
+
+    // Phản hồi thành công
+    res.status(200).json({ message: "Product added successfully" });
   } catch (error) {
-    console.error("Lỗi khi cập nhật đối tác:", error);
-    res.status(500).json({ message: "Không thể cập nhật đối tác" });
+    // Xử lý lỗi
+    console.error("Error adding product:", error);
+    res.status(500).json({ message: "Unable to add product" });
   }
 }
